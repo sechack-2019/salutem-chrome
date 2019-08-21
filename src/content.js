0531fc231b2dc59056ac332b5c9fc326ee7e3d8a
katsuhikotakaishi <ichihiko999@gmail.com>
2019-08-22 01:57:39 +0900

    I DID IT

--- src/content.js
+++ src/content.js
@@ -3,7 +3,29 @@ chrome.extension.onMessage.addListener(function() {
 });
 
 function hide(){
-    product = [...document.getElementsByClassName('sg-col-inner')].slice(4);
-    product[0].className = "suspicious";
+    const product = [...document.getElementsByClassName('sg-col-inner')].slice(4);
+
+    product.forEach((value, index) => {
+        if (index % 7 == 0) {
+            const title = value.querySelectorAll('a')[1].querySelector('span').innerText;
+
+            fetch('https://salutem-api.herokuapp.com/api/v1/post', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    text: title
+                })
+            })
+            .then(res => res.json())
+            .then(res => {
+                if (res.result.id === 1) {
+                    value.className = 'suspicious';
+                }
+            });
+        }
+    });
 };
 
